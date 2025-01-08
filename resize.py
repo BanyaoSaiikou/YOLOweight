2b6c339b15ebd0191a3832bd43774ff89230c629
BanyaoSaiikou <gundamzz1234567@gmail.com>
2025-01-08 20:41:45 +0900

    Delete all files

--- resize.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-image = Image.open("/home/cwh/Desktop/workspace/image/5.png")
-
-resized_img = image.resize((640,480))
-
-resized_img.save("/home/cwh/Desktop/workspace/image/05.png")
